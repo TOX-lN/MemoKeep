3425ea405a6a51ddf57da82e85576f101952b4c7
TOX <saadchair2005@gmail.com>
2026-03-25 23:16:10 +0000

    added main layout with auth protection

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage  = () => {
+    return ( 
+        <div>
+            ONLY for loged in users
+        </div>
+     );
+}
+ 
+export default DocumentsPage ;
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Navigation } from "lucide-react";
+
+const MainLayout = ({
+    children
+}: {children: React.ReactNode
+    
+}) => {
+    const { isAuthenticated, isLoading} = useConvexAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isLoading && !isAuthenticated) {
+            router.push("/")
+        }
+    }, [isAuthenticated, isLoading, router]);
+
+
+
+    if (isLoading) {
+        return (
+            <div className="h-full flex items-center justify-center">
+                <Spinner size="lg" />
+            </div>
+        );
+    }
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+    return (
+        <div className="h-full flex dark:bg-[#1F1F1F]">
+            <main className="flex-1 h_full overflow-y-auto">
+                {children}
+            </main>
+        </div>
+     );
+}
+ 
+export default MainLayout
